6745fbf488f6dbe506705743603a642cb5f35bfe
tomhoon <gnsdl9108@naver.com>
2025-06-19 16:52:09 +0900

    feat: 회원 controller 기능별 추가

--- src/main/java/org/_4/gtree/controller/MemberController.java
+++ src/main/java/org/_4/gtree/controller/MemberController.java
@@ -3,7 +3,9 @@ package org._4.gtree.controller;
 import org._4.gtree.dto.MemberDTO;
 import org._4.gtree.service.MemberService;
 import org._4.gtree.utils.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,4 +30,36 @@ public class MemberController {
 
     return ResponseEntity.ok(ApiResponse.success(dto));
   }
+
+  @PostMapping("/register")
+  public ResponseEntity<ApiResponse<MemberDTO>> register(@RequestBody MemberDTO dto) {
+    MemberDTO res = memberService.registerMember(dto);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(res));
+  }
+
+  @PutMapping("/update")
+  public ResponseEntity<ApiResponse<MemberDTO>> update(@RequestBody MemberDTO dto) {
+    MemberDTO res = memberService.updateMember(dto);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(res));
+  }
+
+  @DeleteMapping("/{mno}")
+  public ResponseEntity<ApiResponse<String>> deleteMember(@PathVariable Long mno) {
+    try {
+      memberService.deleteMember(mno);
+
+      return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("성공적으로 삭제되었습니다."));
+    } catch (Exception e) {
+      log.error("에러발생 {} ", e.getMessage());
+
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("에러발생"));
+    }
+  }
+
 }
