5bc52eea8ca868fd4f64268a99e9c850cc18f2f6
tomhoon <gnsdl9108@naver.com>
2025-06-19 15:07:21 +0900

    first commit

---
+++ src/main/java/org/_4/gtree/GtreeApplication.java
@@ -0,0 +1,13 @@
+package org._4.gtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/_4/gtree/GtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org._4.gtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
